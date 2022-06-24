a2e9e525614ad40b3ded4f8dc8c9314e7641f97c
Hernán Cervera <hjcerveram@hotmail.com>
2022-06-24 10:39:20 -0500

    Verify admin privilege

--- setup.ps1
+++ setup.ps1
@@ -3,6 +3,23 @@
 # Before running, make sure to set the execution policy to unrestricted:
 # set-executionPolicy -scope currentUser unrestricted
 
+function has-admin-privilege {
+  $identity = [system.security.principal.windowsIdentity]::getCurrent()
+  $principal = new-object system.security.principal.windowsprincipal($identity)
+  $admin_role = [system.security.principal.windowsBuiltInRole]::administrator
+  if ($principal.isInRole($admin_role)) {
+    return $true
+  }
+  else {
+    return $false
+  }
+}
+
+if (-not (has-admin-privilege)) {
+  write-error "Please run the script in a session with admin privileges"
+  exit 1
+}
+
 # Pick the user to use for the script and set the 'chosen home'.
 $user = read-host -prompt `
     "Which user do you want to configure? (leave blank to use '$env:username')"
